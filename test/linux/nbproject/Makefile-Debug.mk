a3e885a71b6fdd1c25cdaf5e91553013ece6ad73
Jefty E. Negapatan <jeftyneg@gmail.com>
2011-04-15 00:18:35 +0800

    Added netbeans project to easily edit and build the test application.

---
+++ test/linux/nbproject/Makefile-Debug.mk
@@ -0,0 +1,88 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_CONF=Debug
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/1472/test.o \
+	${OBJECTDIR}/_ext/1445274692/mempoolite.o
+
+
+# C Compiler Flags
+CFLAGS=-Wextra
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-lpthread
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-Debug.mk dist/Debug/test
+
+dist/Debug/test: ${OBJECTFILES}
+	${MKDIR} -p dist/Debug
+	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/test ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/_ext/1472/test.o: ../test.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1472
+	${RM} $@.d
+	$(COMPILE.c) -g -Wall -DMEMPOOLITE_ENABLED=1 -I../../inc -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1472/test.o ../test.c
+
+${OBJECTDIR}/_ext/1445274692/mempoolite.o: ../../src/mempoolite.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1445274692
+	${RM} $@.d
+	$(COMPILE.c) -g -Wall -DMEMPOOLITE_ENABLED=1 -I../../inc -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/1445274692/mempoolite.o ../../src/mempoolite.c
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r build/Debug
+	${RM} dist/Debug/test
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
