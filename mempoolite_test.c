198a6eb36d3774b23c37aa4572eb4ac1725a5df0
Jefty E. Negapatan <jeftyneg@gmail.com>
2011-04-14 22:56:23 +0800

    Added test for multi-threading

--- mempoolite_test.c
+++ mempoolite_test.c
@@ -1,16 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "mempoolite.h"
+
+typedef struct multithreaded_param
+{
+	size_t index;
+	mempoolite_t *pool;
+	size_t min_alloc;
+} multithreaded_param_t;
+
+void *multithreaded_main(void *args)
+{
+	multithreaded_param_t *param;
+	void *buffer;
+
+	param = (multithreaded_param_t *)args;
+
+	while((buffer = mempoolite_malloc(param->pool, param->min_alloc)) != NULL) {
+		printf("index: %u malloc = %p\n", param->index, buffer);
+	}
+
+	return NULL;
+}
 
 int main()
 {
 	size_t counter;
 	size_t buffer_size;
 	size_t min_alloc;
+	size_t num_threads;
 	char *buffer;
 	char *temp;
 	mempoolite_t pool;
+	pthread_t *threads;
+	multithreaded_param_t *threads_param;
 	int test_again;
 
 	/* Get the buffer and size */
@@ -20,18 +45,38 @@ int main()
 		scanf("%u", &buffer_size);
 		printf("Enter the minimum memory allocation size: ");
 		scanf("%u", &min_alloc);
+		printf("Enter the number of threads to run for multi-threaded test: ");
+		scanf("%u", &num_threads);
 		buffer = malloc(buffer_size);
 
 		printf("buffer = %p size = %u minimum alloc = %u\n", buffer, buffer_size,
 			   min_alloc);
 
 		mempoolite_init(&pool, buffer, buffer_size, min_alloc, NULL);
-		printf("Allocation in loop\n");
+		printf("Single-threaded test...\n");
 		counter = 1;
 		while((temp = mempoolite_malloc(&pool, min_alloc)) != NULL) {
 			printf("malloc = %p counter = %u\n", temp, counter);
 			counter++;
 		}
+
+		printf("Multi-threaded test...\n");
+		mempoolite_init(&pool, buffer, buffer_size, min_alloc, NULL);
+		threads = (pthread_t *)malloc(sizeof(*threads) * num_threads);
+		threads_param = (multithreaded_param_t *)malloc(sizeof(*threads_param) * num_threads);
+		/* Run all the threads */
+		for(counter = 0; counter < num_threads; counter++) {
+			threads_param[counter].index = counter;
+			threads_param[counter].min_alloc = min_alloc;
+			threads_param[counter].pool = &pool;
+			pthread_create(&threads[counter], NULL, multithreaded_main, &threads_param[counter]);
+		}
+		/* Wait for all the threads to finish */
+		for(counter = 0; counter < num_threads; counter++) {
+			pthread_join(threads[counter], NULL);
+		}
+		free(threads_param);
+		free(threads);
 
 		free(buffer);
 
